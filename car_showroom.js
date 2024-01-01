3e10bd154785891fc230da53507241706b975d03
Yash <yashaparab222@gmail.com>
2024-01-01 20:53:57 +0530

    Today we changed some themes in navigation bar and and add footer to web.one problem occored while making it responsive

---
+++ car_showroom.js
@@ -0,0 +1,9 @@
+// let menu = document.querySelector("#menu-btn");
+// let navbar = document.querySelector(".navbar");
+
+// menu.onclick = () => {
+//     menu.toggleAttribute("fa-times");
+
+// }
+
+
