2b3b306b4a4e71be6176c9efae4a35de52cea710
Yash <yashaparab222@gmail.com>
2024-01-02 20:05:07 +0530

    We created login page and made web little bit responsive

--- car_showroom.js
+++ car_showroom.js
@@ -1,9 +1,51 @@
-// let menu = document.querySelector("#menu-btn");
-// let navbar = document.querySelector(".navbar");
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
 
-// menu.onclick = () => {
-//     menu.toggleAttribute("fa-times");
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+    document.querySelector('.login-form').classList.toggle('active');
+}
+ 
+document.querySelector('#close-login-form').onclick = () =>{
+    document.querySelector('.login-form').classList.toggle('active');
+}
+
+window.onscroll = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');      
+    }
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onload = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');      
+    }
+
+}
+
+
+
+// function hide() {
+//     let menu = document.getElementById('menu-btn');
+//     console.log("clicked");
+//     let navbar = document.getElementsByClassName('navbar');
+//     if (navbar.style.display != 'none') {
+//         navbar.style.display = 'none';
+//     } else {
+//         navbar.style.display = 'block';
+//     }
 
 // }
-
-
