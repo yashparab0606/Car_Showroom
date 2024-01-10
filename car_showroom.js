bb877f3826fe9ac5b97ebf6dc79d8951e299f0ee
Yash <yashaparab222@gmail.com>
2024-01-11 00:33:04 +0530

    featured section added contaning premium cars

--- car_showroom.js
+++ car_showroom.js
@@ -98,7 +98,37 @@ var swiper = new Swiper(".vehicles-s", {
     slidesPerView: 1,
     spaceBetween: 20,
     loop: true,
-    centeredSlides:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+
+        },
+        768: {
+            slidesPerView: 2,
+
+        },
+        1024: {
+            slidesPerView: 3,
+
+        },
+    },
+});
+
+
+var swiper = new Swiper(".featured-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
